45acd181377c408efc0d125a97416b06b0b18cd5
Mike Lankamp <mike.lankamp@gmail.com>
2008-07-23 12:15:45 +0000

    Added more paranoid Network assertions, fixed CMOVxx instructions sign issue and fixed l3_partial for better distribution
    git-svn-id: https://svn.svp-home.org/MGSim/trunk@505 e97e5017-a994-416e-809d-76780e9f78db

--- Network.cpp
+++ Network.cpp
@@ -128,6 +128,7 @@ Result Network::onSharedReceived(const SharedInfo& sharedInfo)
 
 bool Network::onThreadCleanedUp(GFID fid)
 {
+    assert(fid != INVALID_GFID);
     if (m_cleanedUpThread.empty())
     {
 		DebugSimWrite("Received OnThreadCleanup\n");
@@ -139,6 +140,7 @@ bool Network::onThreadCleanedUp(GFID fid)
 
 bool Network::onThreadCompleted(GFID fid)
 {
+    assert(fid != INVALID_GFID);
     if (m_completedThread.empty())
     {
 		DebugSimWrite("Received OnThreadCompleted\n");
@@ -150,6 +152,7 @@ bool Network::onThreadCompleted(GFID fid)
 
 bool Network::onFamilyCompleted(GFID fid)
 {
+    assert(fid != INVALID_GFID);
     if (m_completedFamily.empty())
     {
 		DebugSimWrite("Received OnFamilyCompleted\n");
@@ -425,7 +428,7 @@ Result Network::onCycleWritePhase(unsigned int stateIndex)
         if (m_sharedResponse.fid != INVALID_GFID && m_sharedResponse.value.m_state == RST_FULL)
         {
             // Response
-			DebugProgWrite("Sending shared %s for G%u; parent: %d\n", m_sharedResponse.addr.str().c_str(), m_sharedResponse.fid, m_sharedResponse.parent);
+			DebugSimWrite("Sending shared %s for G%u; parent: %d\n", m_sharedResponse.addr.str().c_str(), m_sharedResponse.fid, m_sharedResponse.parent);
 			Result res = m_next->onSharedReceived(m_sharedResponse);
 			if (res == FAILED)
             {

ade41e65ae83f03019d14044de84e1f7ffbed220
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-03-29 21:13:13 +0000

    MGSim: add feater to load data after program into memory
    git-svn-id: https://svn.svp-home.org/sw/svp/mg/mgsim/trunk@1864 e97e5017-a994-416e-809d-76780e9f78db

--- src/loader.h
+++ src/loader.h
@@ -20,9 +20,17 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #define LOADER_H
 
 #include "Memory.h"
+#include <algorithm>
 
-// Load the program file into the memory
-Simulator::MemAddr LoadProgram(Simulator::IMemoryAdmin* memory, const std::string& path, bool quiet);
+// Load the program file into the memory;
+// return entry pointer and end address.
+std::pair<Simulator::MemAddr,Simulator::MemAddr>
+ LoadProgram(Simulator::IMemoryAdmin* memory, const std::string& path, bool quiet);
+
+// Load a data file into the memory;
+// load address past the data.
+Simulator::MemAddr LoadDataFile(Simulator::IMemoryAdmin* memory, const std::string& path,
+				Simulator::MemAddr base, bool quiet);
 
 #endif
 

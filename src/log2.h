e891959ecfad123e29ae36e9ddf85264125ab752
Raphael 'kena' Poss <r.c.poss@uva.nl>
2010-08-11 08:51:05 +0000

    [mgsim] add missing file sin previous commit
    git-svn-id: https://svn.svp-home.org/sw/svp/mg/mgsim/trunk@3987 e97e5017-a994-416e-809d-76780e9f78db

---
+++ src/log2.h
@@ -0,0 +1,42 @@
+/*
+mgsim: Microgrid Simulator
+Copyright (C) 2006,2007,2008,2009,2010  The Microgrid Project.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+*/
+#ifndef LOG2_H
+#define LOG2_H
+
+#include <cmath>
+#include "sys_config.h"
+
+#ifndef HAVE_LOG2
+extern "C" {
+    static inline double log2(double x) { return log(x) / M_LN2; }
+}
+#endif
+
+static inline unsigned int ilog2(unsigned long n) 
+{
+	unsigned r = 0;
+	while (n > 1)
+	{
+		r++;
+		n /= 2;
+	}
+	return r;
+}
+
+#endif

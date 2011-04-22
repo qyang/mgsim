681269d57dc96c4954c1cf3bb1954ba5e1552d76
Raphael 'kena' Poss <r.c.poss@uva.nl>
2011-04-22 22:28:23 +0000

    [mgsim-new-io] Add storage for ancillary registers.
    This extends the Processor with an ancillary register file. This
    will be made accessible in a later update through the I/O interface.
    
    git-svn-id: https://svn.svp-home.org/sw/svp/mg/mgsim/branches/new-io@4302 e97e5017-a994-416e-809d-76780e9f78db

---
+++ arch/proc/AncillaryRegisterFile.cpp
@@ -0,0 +1,86 @@
+/*
+mgsim: Microgrid Simulator
+Copyright (C) 2006,2007,2008,2009,2010,2011  The Microgrid Project.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+*/
+#include "Processor.h"
+#include "sim/config.h"
+#include <iomanip>
+
+using namespace std;
+
+namespace Simulator
+{
+    void Processor::AncillaryRegisterInterface::Cmd_Read(std::ostream& out, const std::vector<std::string>& /*arguments*/) const
+    {
+        out << " Register | Value" << endl
+            << "----------+----------------------" << endl;
+
+        size_t numRegisters = GetNumRegisters();
+        for (size_t i = 0; i < numRegisters; ++i)
+        {
+            Integer value;
+            ReadRegister(i, value);
+            out << setw(9) << setfill(' ') << right << i << left
+                << " | "
+                << setw(16 - sizeof(Integer) * 2) << setfill(' ') << ""
+                << setw(     sizeof(Integer) * 2) << setfill('0') << hex << value
+                << endl;
+        }
+    }
+
+
+    bool Processor::AncillaryRegisterFile::ReadRegister(size_t addr, Integer& data) const
+    {
+        if (addr < m_numRegisters)
+        {
+            data = m_registers[addr];
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    bool Processor::AncillaryRegisterFile::WriteRegister(size_t addr, Integer data)
+    {
+        if (addr < m_numRegisters)
+        {
+            m_registers[addr] = data;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    Processor::AncillaryRegisterFile::AncillaryRegisterFile(const std::string& name, Processor& parent, Clock& clock, const Config& config)
+        : AncillaryRegisterInterface(name, parent, clock),
+          m_numRegisters(config.getValue<size_t>("NumAncillaryRegisters", 8))
+    {
+        if (m_numRegisters == 0)
+        {
+            throw InvalidArgumentException("NumAncillaryRegisters must be 1 or larger");
+        }
+
+        m_registers.resize(m_numRegisters, 0);
+    }
+
+
+}
---
+++ arch/proc/AncillaryRegisterFile.h
@@ -0,0 +1,71 @@
+/*
+mgsim: Microgrid Simulator
+Copyright (C) 2006,2007,2008,2009,2010,2011  The Microgrid Project.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+*/
+#ifndef ANCILLARYREGISTERFILE_H
+#define ANCILLARYREGISTERFILE_H
+
+
+#ifndef PROCESSOR_H
+#error This file should be included in Processor.h
+#endif
+
+/*
+ * Access interface to ancillary registers. Ancillary registers
+ * differ from regular (computational) registers in that
+ * they are few and do not have state bits. 
+ * FIXME:
+ * This should really be a register file with ports and arbitration etc.
+ * Accessed R/W from the memory stage, W from delegation (remote configure)
+ */
+
+
+/* The interface is shared between ancillary core registers and
+   performance counters. */
+class AncillaryRegisterInterface : public Object
+{
+public:
+    AncillaryRegisterInterface(const std::string& name, Object& parent, Clock& clock)
+        : Object(name, parent, clock) {}
+
+    virtual size_t GetNumRegisters() const = 0;
+    virtual bool ReadRegister(size_t addr, Integer& data) const = 0;
+    virtual bool WriteRegister(size_t addr, Integer data) = 0;
+    
+    void Cmd_Read(std::ostream& out, const std::vector<std::string>& arguments) const;
+    
+
+    virtual ~AncillaryRegisterInterface() {}
+};
+
+class AncillaryRegisterFile : public AncillaryRegisterInterface
+{
+    const size_t                  m_numRegisters;
+    std::vector<Integer>          m_registers;
+
+public:
+    AncillaryRegisterFile(const std::string& name, Processor& parent, Clock& clock, const Config& config);
+    
+    size_t GetNumRegisters() const { return m_numRegisters; }
+
+    bool ReadRegister(size_t addr, Integer& data) const;
+    bool WriteRegister(size_t addr, Integer data);
+
+};
+
+
+#endif

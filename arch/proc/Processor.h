a5f98d77afe28ba83dd3f83df8f180e586676368
Raphael 'kena' Poss <r.c.poss@uva.nl>
2011-06-28 09:57:04 +0000

    [mgsim] Allow to configure the bank selection for the L1 D-Cache.
    git-svn-id: https://svn.svp-home.org/sw/svp/mg/mgsim/trunk@4585 e97e5017-a994-416e-809d-76780e9f78db

--- arch/proc/Processor.h
+++ arch/proc/Processor.h
@@ -22,6 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #include "sim/inspect.h"
 #include "arch/IOBus.h"
 #include "arch/Memory.h"
+#include "arch/BankSelector.h"
 
 class Config;
 
